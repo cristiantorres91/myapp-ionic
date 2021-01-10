b2b71840c8a27ca29550c87370b0d10e0fd78e43
cristiantorres91 <49564966+cristiantorres91@users.noreply.github.com>
2021-01-10 17:23:06 -0600

    Primer comit app ionic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,90 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+      <ion-toolbar color="primary">
+        <ion-title>App Users</ion-title>
+        <ion-title class="ion-text-right" size="small">Cristian Torres</ion-title>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
+    <ion-content :fullscreen="true"> 
+      <ion-list>
+        <ion-list-header lines="inset">
+          <ion-label>Users</ion-label>
+        </ion-list-header>
+      <div v-for="item in dataApi"  :key="item.id">
+          <ion-item >
+            <ion-avatar slot="start">
+              <ion-icon style="color:#26a660" :icon="personCircleOutline" size="large"/>
+            </ion-avatar>
+            <ion-label>
+              <h2>{{ item.name }}</h2>
+              <h3>{{ item.email }}</h3>
+            </ion-label>
+          </ion-item>
+        </div>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonListHeader,
+  IonLabel,
+  IonAvatar,
+  IonItem,
+  IonIcon,
+} from "@ionic/vue";
+import { defineComponent, ref, onMounted } from "vue";
+import { personCircleOutline } from "ionicons/icons";
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonList,
+    IonListHeader,
+    IonLabel,
+    IonAvatar,
+    IonItem,
+    IonIcon,
+  },
+  setup() {
+    const dataApi = ref([]);
+    //metodo para obtener la data de la api
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        dataApi.value = await response.json();
+        console.log(dataApi.value);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    //llamamos metodo para cargar la data
+    onMounted(() => {
+      getData();
+    });
+    return {
+      dataApi,
+      getData,
+      personCircleOutline
+    };
+  },
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
